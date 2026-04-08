742b985df9accde5d512e9e7c5f3c149386290c0
abdelahdiDevweb <abdouhadi2002@gmail.com>
2026-04-08 08:13:20 +0100

    gives access

--- index.ts
+++ index.ts
@@ -43,7 +43,8 @@ app.set("trust proxy", 1);
 // Initialize Socket.IO
 const io = new SocketIOServer(server, {
   cors: {
-    origin: process.env.NODE_ENV !== 'production' ? true : ["http://localhost:3000", "http://localhost:5173"],
+    origin: true,
+    // origin: process.env.NODE_ENV !== 'production' ? true : ["http://localhost:3000", "http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST"]
   },
