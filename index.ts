7b96a4f9b822c15a6da0ecb9748a198c10b2b1ae
abdelahdiDevweb <abdouhadi2002@gmail.com>
2026-04-09 15:34:04 +0100

    add url

--- index.ts
+++ index.ts
@@ -43,7 +43,7 @@ app.set("trust proxy", 1);
 // Initialize Socket.IO
 const io = new SocketIOServer(server, {
   cors: {
-    origin: true,
+    origin:"*",
     // origin: process.env.NODE_ENV !== 'production' ? true : ["http://localhost:3000", "http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST"]
